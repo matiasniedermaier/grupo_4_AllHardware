ebacc475ba9cc455ca89e386894f9bcfe0b029c7
licia90 <liciacoronel06@gmail.com>
2020-05-27 19:23:18 -0300

    instalacion de modulos

--- paginaAllHardware/site/routes/login.js
+++ paginaAllHardware/site/routes/login.js
@@ -1,8 +1,12 @@
 const expres = require('express');
 const router = expres.Router();
-const formulario = require('../controller/registroController')
+const {check, validationResult, body} =  require('express-validator');
+const formulario = require('../controller/registroController');
 
 router.get('/', formulario.login);
-router.post('/', formulario.login);
+
+router.post('/',[check('email').isEmail()],[check('email').isEmpty()], formulario.login);
+console.log('paso por aqui');
+
 
 module.exports = router;
