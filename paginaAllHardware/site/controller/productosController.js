4aacd17591cddf4472593b08abc5b298729c56b4
Matias Niedermaier <matiasnieder@gmail.com>
2020-06-07 17:58:33 -0300

    edit en controller para a mostrar vista y agrego en el menu desplegrable el acceso para nuevo producto

--- paginaAllHardware/site/controller/productosController.js
+++ paginaAllHardware/site/controller/productosController.js
@@ -97,7 +97,7 @@ let productosController = {
         let productoAMostrar = archivoProductos.filter(function (productos) {
             return req.params.id == productos.id;
         });
-        res.render('editTes', {productosMostrar:productoAMostrar});
+        res.render('edit', {productosMostrar:productoAMostrar});
 
     },
 
