6413eafa2d95e986ccf34831b1c8d358496a9a47
Matias Niedermaier <matiasnieder@gmail.com>
2020-08-09 16:10:45 -0300

    nuevos seeders

--- paginaAllHardware/site/database/seeders/20200725230455-brands.js
+++ paginaAllHardware/site/database/seeders/20200725230455-brands.js
@@ -4,10 +4,17 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('brands', [
       {id: 1, name:"Intel"},
-      {id: 2, name:"Amd"},
+      {id: 2, name:"AMD"},
       {id: 3, name:"Asus"},
       {id: 4, name:"Lg"},
       {id: 5, name:"Nokia"},
+      {id: 6, name:"Kingston"},
+      {id: 7, name:"Samsung"},
+      {id: 8, name:"Corsair"},
+      {id: 9, name:"Thermaltake"},
+      {id: 10, name:"MSI"},
+      {id: 11, name:"BenQ"},
+      {id: 12, name:"Logitech"}
 ])
   },
   down: async (queryInterface, Sequelize) => {
--- paginaAllHardware/site/database/seeders/20200725230542-category.js
+++ paginaAllHardware/site/database/seeders/20200725230542-category.js
@@ -3,11 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('category', [
-      {id: 1, name:"Procesadores"},
-      {id: 2, name:"Memoria Ram"},
-      {id: 3, name:"Fuentes"},
-      {id: 4, name:"Cargadores"},
-      {id: 5, name:"Estrella"},
+      {id: 1, name:"Almacenamiento"},
+      {id: 2, name:"Fuentes"},
+      {id: 3, name:"Gabinete"},
+      {id: 4, name:"Memorias Ram"},
+      {id: 5, name:"Monitores"},
+      {id: 6, name:"Motherboards"},
+      {id: 7, name:"Perifericos"},
+      {id: 8, name:"Placas de Video"},
+      {id: 9, name:"Procesadores"},
+      {id: 10, name:"Refrigeración"},
    ])
   },
   down: async (queryInterface, Sequelize) => {
